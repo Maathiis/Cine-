5cbb5444b02d447a7b13efa2edce4845a4a16235
tsavou <theosavourat@gmail.com>
2025-05-14 14:52:18 +0200

    fix conflicts

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -78,17 +78,17 @@
         Ciné+
       </a>
 
-      <button
+      <input
         class="text-white focus:outline-none p-1"
         on:click={toggleSearch}
 				bind:value={query}
 				on:keydown={handleKeyDown}
         aria-label="Rechercher"
-      >
+      />
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
         </svg>
-      </button>
+
     </nav>
 
     <!-- Mobile Search Bar -->
